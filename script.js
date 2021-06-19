73b440f204b431e13c9cc13fadb1bf23275798d8
FilipeGuto <filipeguto95@gmail.com>
2021-06-19 11:41:41 -0300

    Projeto Trybewarts

---
+++ script.js
@@ -0,0 +1,32 @@
+function validacaoEmailSenha() {
+  const login = document.getElementById('login').value;
+  const senha = document.getElementById('senha').value;
+  const loginPadrao = 'tryber@teste.com';
+  const senhaPadrao = '123456';
+  if (login !== loginPadrao || senha !== senhaPadrao) {
+    alert('Login ou senha inválidos.');
+  } else {
+    alert('Olá, Tryber!');
+  }
+}
+document.querySelector('.login').addEventListener('click', validacaoEmailSenha);
+
+function hideApear() {
+  const checkboxEnviar = document.getElementById('agreement');
+  const submit = document.getElementById('submit-btn');
+  if (checkboxEnviar.checked !== true) {
+    submit.setAttribute('disabled', true);
+  } else {
+    submit.removeAttribute('disabled');
+  }
+}
+document.getElementById('agreement').addEventListener('click', hideApear);
+
+function newForm() {
+  const name = document.getElementById('input-name').value;
+  localStorage.setItem(name);
+  const lastname = document.getElementById('input-lastname').value;
+  localStorage.setItem(lastname).value = 'Aqui';
+}
+
+document.getElementById('submit-btn').addEventListener('click', newForm);
